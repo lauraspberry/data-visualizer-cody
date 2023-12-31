8115f908a2e032dd94e23aef8807fd9f6cff4c28
Laura Pei <lauraspberrypie@gmail.com>
2023-12-30 19:33:36 -0800

    implement backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +26,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!! 
+          Learn React
         </a>
+
+        <p>The current time is {currentTime}.</p>
+
       </header>
     </div>
   );
